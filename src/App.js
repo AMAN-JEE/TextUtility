e5ca93de309b767ea6814a712df5ed78d62f5951
AMAN-JEE <kumaraman15856@gmail.com>
2023-07-06 22:00:41 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Textarea from "./components/Textarea";
 import { useState } from "react";
+import About from "./components/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState("light");  
+  const [mode, setMode] = useState("light");
   function toggleMode() {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#081a35";
-
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
     }
   }
+
   return (
-    <>
-      <Navbar title="TextConvert" mode={mode} toggleMode={toggleMode}/>
-      <Textarea heading="Enter the text below to Convert" mode={mode}/>
-    </>
+    <Router>
+      <Navbar title="TextConvert" mode={mode} toggleMode={toggleMode} />
+      <Switch>
+        <Route path="/">
+          <Textarea heading="Enter the text below to Convert" mode={mode} />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
